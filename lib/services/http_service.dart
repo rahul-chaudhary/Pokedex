2c48ca46cb19a76d7661f35eb067f9464b01c302
Rahul Chaudhary <bossrc64@outlook.com>
2025-01-23 20:11:36 +0530

    SETUP: The class for HTTP services

---
+++ lib/services/http_service.dart
@@ -0,0 +1,18 @@
+import 'package:dio/dio.dart';
+import '../utils/helpers/debug_print.dart';
+
+class HTTPService {
+  HTTPService();
+
+  final _dio = Dio();
+
+  Future<Response?> get({required String path}) async {
+    try{
+    Response res = await _dio.get(path);
+    } catch(e){
+      dbPrint(e);
+    }
+
+    return null;
+  }
+}
