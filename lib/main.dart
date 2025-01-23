d379b9f49b4487885fc57c097916e9699b9e6b95
Rahul Chaudhary <bossrc64@outlook.com>
2025-01-23 20:13:16 +0530

    ADDED: setUpServices function in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:get_it/get_it.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:pokedex/pages/home_page.dart';
+import 'package:pokedex/services/http_service.dart';
 
-void main() {
+void main() async {
+  await _setUpServices();
   runApp(const MyApp());
+}
+
+Future<void> _setUpServices() async {
+  GetIt.instance.registerSingleton<HTTPService>(HTTPService());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return ProviderScope(
