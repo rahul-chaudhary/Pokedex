4c2805e72df4574c1656e8a7d2d4b5e317d7331a
Rahul Chaudhary <bossrc64@outlook.com>
2025-01-23 22:28:56 +0530

    UPDATED: home_page_controller.dart

--- lib/controllers/home_page_controller.dart
+++ lib/controllers/home_page_controller.dart
@@ -1,8 +1,28 @@
+import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:get_it/get_it.dart';
+import 'package:pokedex/services/http_service.dart';
+import 'package:pokedex/utils/helpers/debug_print.dart';
 import '../models/page_data.dart';
 
 class HomePageController extends StateNotifier<HomePageData> {
-  HomePageController(super.state);
 
+  final GetIt _getIt = GetIt.instance;
+  late HTTPService _httpService;
+  static const pokeAPIURL = 'https://pokeapi.co/api/v2/pokemon?limit=20&offset=0';
+  HomePageController(super.state) {
+    _httpService = _getIt.get<HTTPService>();
+    _setup();
+  }
 
+  Future<void> _setup() async {
+    _loadData();
+  }
+
+  Future<void> _loadData() async {
+    if(state.data == null) {
+     Response? response = await _httpService.get(path: pokeAPIURL);
+     dbPrint('_loadData response ${response?.data}');
+    } else{}
+  }
 }
