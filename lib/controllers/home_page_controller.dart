7f79ab4d0555f88643df3631155474c96d1f1c50
Rahul Chaudhary <bossrc64@outlook.com>
2025-01-23 19:44:25 +0530

    CONTROLLER: For the HomePage

---
+++ lib/controllers/home_page_controller.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../models/page_data.dart';
+
+class HomePageController extends StateNotifier<HomePageData> {
+  HomePageController(super.state);
+
+
+}
