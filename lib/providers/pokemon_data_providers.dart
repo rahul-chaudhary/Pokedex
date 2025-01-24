863c76ec77367753dbd82e561841a3bd3dc3d4a0
Rahul Chaudhary <bossrc64@outlook.com>
2025-01-24 14:44:53 +0530

    ADDED: database services

--- lib/providers/pokemon_data_providers.dart
+++ lib/providers/pokemon_data_providers.dart
@@ -2,7 +2,9 @@ import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:get_it/get_it.dart';
 import 'package:pokedex/models/pokemon.dart';
+import 'package:pokedex/services/database_service.dart';
 import 'package:pokedex/services/http_service.dart';
+import 'package:pokedex/utils/constants/app_key.dart';
 
 final pokemonDataProvider = FutureProvider.family<Pokemon?, String>((ref, url) async {
   HTTPService _httpService = GetIt.instance.get<HTTPService>();
@@ -19,17 +21,22 @@ final favPokemonProvider = StateNotifierProvider<FavPokemonProvider, List<String
   return FavPokemonProvider([]);
 });
 class FavPokemonProvider extends StateNotifier<List<String>> {
+
+  final DatabaseService _databaseService = GetIt.instance.get<DatabaseService>();
   FavPokemonProvider(super.state) {
     _setup();
   }
   Future<void> _setup() async {
-
+    List<String>? result = await _databaseService.getList(AppKey.favPokemonListKey);
+    state = result ?? [];
   }
 
   void addFavPokemon(String url) {
     state = [...state, url];
+    _databaseService.saveList(AppKey.favPokemonListKey, state);
   }
   void removeFavPokemon(String url) {
     state = state.where((element) => element != url).toList();
+    _databaseService.saveList(AppKey.favPokemonListKey, state);
   }
 }
